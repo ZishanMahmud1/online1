14c6ffe5b2b006f620cf533b0369a5863e88c92d
shaon <fshaon183067@bscse.uiu.ac.bd>
2022-03-21 14:44:25 +0600

    leave_input is commited in branch applyforleave

---
+++ leave_input.php
@@ -0,0 +1,20 @@
+<h1>Apply for leave form</h1>
+
+
+
+<form method=get action=create_result.php>
+
+	 employee's name:<input type=text name=name> <br>
+
+	<p>
+
+	leave start: <input type=text name=startdate> <br>
+
+	<p>
+    
+    leave end: <input type=text name=endtdate> <br> 
+   
+
+	<input type=submit value=Insert>
+
+</form>
